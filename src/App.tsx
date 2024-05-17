f753d24fd691c4f6b64827f282aea798f5743a35
Renata <renatasilvahan@gmail.com>
2024-05-17 19:29:25 -0300

    chore:finalizando tirar foto e inclusao da navegaçao

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,57 @@
+// import 'react-native-gesture-handler';
 import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+// import Icon from 'react-native-vector-icons/Ionicons';
+// import {SafeAreaView, StatusBar, StyleSheet} from 'react-native';
 
-import {SafeAreaView, StatusBar, StyleSheet} from 'react-native';
+// import {PaperProvider} from 'react-native-paper';
 
-import {PaperProvider} from 'react-native-paper';
-
-import Stories from './components/Stories';
+import Login from './components/Login';
+import UserPage from './components/UserPage';
 import Feed from './components/Feed';
-import Camera from './components/Camera';
-// import UserPage from './components/UserPage';
-// import Login from './components/Login';
+import CameraScreen from './components/CameraScreen';
 
-const App = () => {
-  return (
-    <PaperProvider>
-      <SafeAreaView style={styles.container}>
-        <Camera />
-        <StatusBar backgroundColor="#fff" barStyle="dark-content" />
-        {/* <Login /> */}
-        <Stories />
-        {/* <Foto /> */}
-        <Feed />
-        {/* <UserPage /> */}
-      </SafeAreaView>
-    </PaperProvider>
-  );
+type RootStackParamList = {
+  Login: undefined;
+  Home: undefined;
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#6A5ACD',
-  },
-});
+type TabParamList = {
+  Perfil: undefined;
+  Feed: undefined;
+  Câmera: undefined;
+  Pagamento: undefined;
+};
+
+const Stack = createStackNavigator<RootStackParamList>();
+const Tab = createBottomTabNavigator<TabParamList>();
+
+const App: React.FC = () => (
+  <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Home" component={HomeNavigator} />
+    </Stack.Navigator>
+  </NavigationContainer>
+);
+
+const HomeNavigator: React.FC = () => (
+  <Tab.Navigator>
+    <Tab.Screen name="Perfil" component={UserPage} />
+    <Tab.Screen name="Feed" component={Feed} />
+    <Tab.Screen
+      name="Câmera"
+      component={CameraScreen}
+      // options={{
+      //   tabBarIcon: ({color, size}) => (
+      //     <Icon name="camera" size={size} color={color} />
+      //   ),
+      // }}
+    />
+    <Tab.Screen name="Pagamento" component={PaymentScreen} />
+  </Tab.Navigator>
+);
+
 export default App;
---
+++ src/components/UserPage.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import {View} from 'react-native';
+import {Text} from 'react-native-paper';
+
+const UserPage = () => {
+  return (
+    <View>
+      <Text>Página do usuário</Text>
+    </View>
+  );
+};
+
+export default UserPage;
