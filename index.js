2d2cd96eacef2a4fbe03918f2ce27a0828ed07b5
Renata <renatasilvahan@gmail.com>
2024-06-23 23:15:09 -0300

    chore: autenticacao com supabase

--- index.js
+++ index.js
@@ -3,7 +3,20 @@
  */
 
 import {AppRegistry} from 'react-native';
+import React from 'react';
 import App from './src/App';
 import {name as appName} from './app.json';
+import {PaperProvider} from 'react-native-paper';
+import {AuthProvider} from './src/provider/AuthProvider';
 
-AppRegistry.registerComponent(appName, () => App);
+function Main() {
+  return (
+    <PaperProvider>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </PaperProvider>
+  );
+}
+
+AppRegistry.registerComponent(appName, () => Main);
